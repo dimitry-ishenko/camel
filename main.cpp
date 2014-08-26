76123cfff64659c1d22b2461b4af57b9acf54aa6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2014-08-25 21:19:03 -0400

    Launch manager

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-#include <QApplication>
+#include "manager.h"
+#include <string>
+#include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+const std::string usage= "Usage: camel [ -h | --help | <path-to-config-file> ]";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
-    return 0;
+    if(argc > 2)
+    {
+        std::cerr << usage << std::endl;
+        return 1;
+    }
+
+    QString arg;
+    if(argc==2)
+    {
+        arg= argv[1];
+        if(arg == "-h" || arg == "--help")
+        {
+            std::cout << usage << std::endl;
+            return 0;
+        }
+    }
+
+    return Manager(arg).exec();
 }
